80eadc0208d505360e39b83f4c7712e02eaba69d
Dolapo <checdolzee3@gmail.com>
2026-06-16 14:37:58 +0100

    just finished enabling routing and setting up app routes on index.js and App.jsx respectively

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import PostDetail from './pages/PostDetail';
+import Header from './components/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      
-    </>
-  )
+   <div>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path ="/post/:id" element ={<PostDetail/>}/>
+    </Routes>
+   </div>
+  );
 }
 
-export default App
+export default App;
