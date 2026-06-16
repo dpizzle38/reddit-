11d9ca638839fc3222ad997d8d83f81772a3f54a
Dolapo <checdolzee3@gmail.com>
2026-06-16 23:58:43 +0100

    Just finished working on PostDetail and Header Compoments respectively

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
+import {Link} from 'react-router-dom'
 
+
+export default function Header(){
+   return(
+    <header>
+     <Link to='/'>
+     <h1>Reddit Client</h1>
+     </Link>
+    </header>
+   )
+}
---
+++ src/pages/PostDetail.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect} from 'react-router-dom';
+import { useParams } from "react-router-dom";
+import { fetchPostComments} from '../services/redditApi';
+
+export default function postDetail(){
+    const {id} = useParams();
+    const [comments, setComments] = useState([]);
+
+  useEffect(()=>{
+    const loadComments = async()=>{
+    const data = await fetchPostComments(id)
+    setComments(data)
+}
+
+    loadComments()
+  }, [id])
+}
+ return(
+    <div>
+        <h2>postDetail</h2>
+        <h3>comments</h3>
+   
+    {comments.map((comment)=>
+    (
+        <div key={comment.id}>
+          <p>{comment.body}</p>
+        </div>
+    )
+    )}
+    </div>
+ )
